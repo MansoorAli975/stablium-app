06e018d39eae4c56e9664498e94a465650c93ba9
Mansoor Ali <mansoor.ali.975@gmail.com>
2024-10-23 12:21:40 +0500

    Save changes before installing OpenZeppelin contracts

---
+++ test/fuzz/Handler.t.sol
@@ -0,0 +1,2 @@
+// Handler is going to narrow down the way we call functions.
+// Purpose is to save and optimize the usge of our runs without wasting them.
---
+++ test/fuzz/InvariantsTest.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+// This file has our varinats / properties.
+// What are our variants?
+//   1. The total supply of DSC should be less than the total value of collateral.
+//   2. Getter view functions should never revert.
+
+pragma solidity ^0.8.18;
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+
+
+contract InvariantsTest is StdInvariant, Test {
+
+DeployDSC deployer;
+DSCEngine dsce;
+    function setUp() external {
+
+        deployer = new DeployDSC();
+        dsce = deployer.run();
+    }
+
+}
